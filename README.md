969e76091af9dea2479478e50f26bc0bd0cacd90
Kelvin Zhao <icyaneon@gmail.com>
2022-12-19 15:32:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# website-porfolio
+My personal website!
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './styles/App.css';
+import React from 'react';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div class="flex justify-center items-center h-screen">
+      <h1 class="text-6xl">Hi, I'm Kelvin</h1>
+    </div>
+  );
+}
+
+export default App;
